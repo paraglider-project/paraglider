ae38be48d07194f1039f973206d6337e03626914
Sarah McClure <sarahmcclure97@gmail.com>
2023-07-03 10:44:47 -0700

    remove unused code in test plugin

--- src/test_plugin/test_plugin.go
+++ src/test_plugin/test_plugin.go
@@ -10,14 +10,6 @@ import (
 	"github.com/NetSys/invisinets/src/invisinetspb"
   )
 
-var (
-	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
-	certFile   = flag.String("cert_file", "", "The TLS cert file")
-	keyFile    = flag.String("key_file", "", "The TLS key file")
-	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
-	port       = flag.Int("port", 50051, "The server port")
-)
-
 type cloudPluginServer struct {
 	invisinetspb.UnimplementedCloudPluginServer
 }
@@ -50,20 +42,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	var opts []grpc.ServerOption
-	// if *tls {
-	// 	if *certFile == "" {
-	// 		*certFile = data.Path("x509/server_cert.pem")
-	// 	}
-	// 	if *keyFile == "" {
-	// 		*keyFile = data.Path("x509/server_key.pem")
-	// 	}
-	// 	creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
-	// 	if err != nil {
-	// 		log.Fatalf("Failed to generate credentials: %v", err)
-	// 	}
-	// 	opts = []grpc.ServerOption{grpc.Creds(creds)}
-	// }
-	grpcServer := grpc.NewServer(opts...)
+	grpcServer := grpc.NewServer(opts)
 	invisinetspb.RegisterCloudPluginServer(grpcServer, newServer())
 	grpcServer.Serve(lis)
 }
