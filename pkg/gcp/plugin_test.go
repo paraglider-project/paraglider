9ec8224171a1c42430056baad834d415cb3d85bf
Sean Kim <kimsean@google.com>
2023-07-19 23:53:33 +0000

    Working all three tests

--- pkg/gcp/plugin_test.go
+++ pkg/gcp/plugin_test.go
@@ -30,110 +30,173 @@ import (
 	invisinetspb "github.com/NetSys/invisinets/pkg/invisinetspb"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/api/option"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 )
 
-const fakeResourceId = "invisinets-test/us-west1-a/vm-invisinets-test"
+const fakeProject = "invisinets-test"
+const fakeZone = "us-west1-a"
+const fakeInstance = "vm-invisinets-test"
+const fakeResourceId = fakeProject + "/" + fakeZone + "/" + fakeInstance
 
-var fakeFirewalls = []*computepb.Firewall{
-	{
-		Allowed: []*computepb.Allowed{
-			{
-				IPProtocol: proto.String("6"),
-				Ports:      []string{"80"},
-			},
-		},
-		Direction:    proto.String(computepb.Firewall_INGRESS.String()),
-		Disabled:     proto.Bool(false),
-		Name:         proto.String("fw-invisinets-permitlist-a48c81a983da600d848e5d35d8ad05cc90b41"),
-		Network:      proto.String("nw-invisinets"),
-		SourceRanges: []string{"10.1.2.0/24"},
-		TargetTags:   []string{"invisinets-permitlist-1dcfb54806337196becb956a7566c9b4a1de9cd40"}, // TODO @seankimkdy: update to actual
-		// TODO @seankimkdy: add SourceTags test once we start supporting tags
-	},
-	{
-		Allowed: []*computepb.Allowed{
-			{
-				IPProtocol: proto.String("17"),
-				Ports:      []string{},
-			},
-		},
-		DestinationRanges: []string{"10.3.4.0/24"},
-		Direction:         proto.String(computepb.Firewall_EGRESS.String()),
-		Disabled:          proto.Bool(false),
-		Name:              proto.String("fw-invisinets-permitlist-cc2ee3ff1e19a62b32c07e54c23c42ebbdb32"), // TODO @seankimkdy: update to actual name
-		Network:           proto.String("nw-invisinets"),
-		TargetTags:        []string{"invisinets-permitlist-1dcfb54806337196becb956a7566c9b4a1de9cd40"},
-	},
-	{
-		Allowed: []*computepb.Allowed{
-			{
-				IPProtocol: proto.String("6"),
-				Ports:      []string{"22"},
-			},
-		},
-		Direction:  proto.String(computepb.Firewall_INGRESS.String()),
-		Disabled:   proto.Bool(false),
-		Name:       proto.String("fw-invisinets-permitlist-allow-ssh"),
-		Network:    proto.String("global/networks/default"),
-		TargetTags: []string{"0.0.0.0/0"},
-	},
-	{
-		Allowed: []*computepb.Allowed{
-			{
-				IPProtocol: proto.String("1"),
-				Ports:      []string{},
-			},
-		},
-		Direction:  proto.String(computepb.Firewall_INGRESS.String()),
-		Disabled:   proto.Bool(false),
-		Name:       proto.String("fw-allow-icmp"),
-		Network:    proto.String("nw-invisinets"),
-		TargetTags: []string{"0.0.0.0/0"},
-	},
+const fakeTag = "invisinets-permitlist-1dcfb54806337196becb956a7566c9b4a1de9cd40"
+
+type fakeServerState struct {
+	firewallMap map[string]*computepb.Firewall
 }
 
-var fakeServerHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("%s\n", r.URL.Path)
-	switch path := r.URL.Path; path {
-	case "/compute/v1/projects/invisinets-test/global/firewalls":
-		if r.Method == "POST" {
-			body, err := io.ReadAll(r.Body)
-			if err != nil {
-				http.Error(w, "could not read body of request: "+err.Error(), http.StatusBadRequest)
+func getFakeServerHandler(fakeServerState *fakeServerState) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// TODO @seankimkdy: cleanup duplicate code regarding marshalling/unmarshaling
+		fmt.Printf("%+v\n", r.URL)
+		switch path := r.URL.Path; path {
+		case "/compute/v1/projects/invisinets-test/global/firewalls":
+			if r.Method == "POST" {
+				body, err := io.ReadAll(r.Body)
+				if err != nil {
+					http.Error(w, "unable to read body of request: "+err.Error(), http.StatusBadRequest)
+					return
+				}
+				var firewall computepb.Firewall
+				unm := protojson.UnmarshalOptions{AllowPartial: true, DiscardUnknown: true}
+				if err = unm.Unmarshal(body, &firewall); err != nil {
+					http.Error(w, "unable to unmarshal body of request: "+err.Error(), http.StatusBadRequest)
+					return
+				}
+				fakeServerState.firewallMap[*firewall.Name] = &firewall
+				resp := &computepb.Operation{}
+				b, err := json.Marshal(resp)
+				if err != nil {
+					http.Error(w, "unable to marshal request: "+err.Error(), http.StatusBadRequest)
+					return
+				}
+				w.Write(b)
 				return
 			}
-			fmt.Printf("Byte array inside server looks like:\n%v\n", body)
-			var f computepb.Firewall
-			err = json.Unmarshal(body, &f)
-			if err != nil {
-				http.Error(w, "unable to unmarshal request body: "+err.Error(), http.StatusBadRequest)
+		// TODO @seankimkdy: don't hardcode and figure out how to regex match
+		case "/compute/v1/projects/invisinets-test/global/firewalls/fw-invisinets-permitlist-a48c81a983da600d848e5d35d8ad05cc90b41":
+			if r.Method == "DELETE" {
+				delete(fakeServerState.firewallMap, "fw-invisinets-permitlist-a48c81a983da600d848e5d35d8ad05cc90b41")
+				resp := &computepb.Operation{}
+				b, err := json.Marshal(resp)
+				if err != nil {
+					http.Error(w, "unable to marshal request: "+err.Error(), http.StatusBadRequest)
+					return
+				}
+				w.Write(b)
 				return
 			}
-			fmt.Printf("%+v\n", &f)
-			fakeFirewalls = append(fakeFirewalls, &f)
-		}
-		return
-	case "/compute/v1/projects/invisinets-test/zones/us-west1-a/instances/vm-invisinets-test/getEffectiveFirewalls":
-		resp := &computepb.InstancesGetEffectiveFirewallsResponse{
-			FirewallPolicys: nil,
-			Firewalls:       fakeFirewalls,
-		}
-		b, err := json.Marshal(resp)
-		if err != nil {
-			http.Error(w, "unable to marshal request: "+err.Error(), http.StatusBadRequest)
+		case fmt.Sprintf("/compute/v1/projects/%v/zones/%v/instances/%v/getEffectiveFirewalls", fakeProject, fakeZone, fakeInstance):
+			if r.Method == "GET" {
+				firewalls := make([]*computepb.Firewall, 0, len(fakeServerState.firewallMap))
+				for _, value := range fakeServerState.firewallMap {
+					firewalls = append(firewalls, value)
+				}
+				resp := &computepb.InstancesGetEffectiveFirewallsResponse{
+					FirewallPolicys: nil,
+					Firewalls:       firewalls,
+				}
+				b, err := json.Marshal(resp)
+				if err != nil {
+					http.Error(w, "unable to marshal request: "+err.Error(), http.StatusBadRequest)
+					return
+				}
+				w.Write(b)
+				return
+			}
+		case fmt.Sprintf("/compute/v1/projects/%v/zones/%v/instances/%v", fakeProject, fakeZone, fakeInstance):
+			if r.Method == "GET" {
+				resp := &computepb.Instance{Tags: &computepb.Tags{Items: []string{fakeTag}}}
+				b, err := json.Marshal(resp)
+				if err != nil {
+					http.Error(w, "unable to marshal request: "+err.Error(), http.StatusBadRequest)
+					return
+				}
+				w.Write(b)
+				return
+			}
+		case fmt.Sprintf("/compute/v1/projects/%v/global/operations/", fakeProject):
+			// TODO @seankimkdy: add comment about operation not having a name here?
+			if r.Method == "GET" {
+				resp := &computepb.Operation{
+					Status: computepb.Operation_DONE.Enum(),
+				}
+				b, err := json.Marshal(resp)
+				if err != nil {
+					http.Error(w, "unable to marshal request: "+err.Error(), http.StatusBadRequest)
+					return
+				}
+				w.Write(b)
+				return
+			}
+		default:
+			http.Error(w, "unsupported URL and/or method", http.StatusBadRequest)
 			return
 		}
-		w.Write(b)
-		return
-	default:
-		http.Error(w, "unrecognized URL for test", http.StatusBadRequest)
-		return
+	})
+}
+
+func generateFakeServerState() *fakeServerState {
+	return &fakeServerState{
+		firewallMap: map[string]*computepb.Firewall{
+			"fw-invisinets-permitlist-a48c81a983da600d848e5d35d8ad05cc90b41": {
+				Allowed: []*computepb.Allowed{
+					{
+						IPProtocol: proto.String("6"),
+						Ports:      []string{"80"},
+					},
+				},
+				Direction:    proto.String(computepb.Firewall_INGRESS.String()),
+				Name:         proto.String("fw-invisinets-permitlist-a48c81a983da600d848e5d35d8ad05cc90b41"), // generated by getFirewallName
+				Network:      proto.String("nw-invisinets"),
+				SourceRanges: []string{"10.1.2.0/24"},
+				TargetTags:   []string{fakeTag},
+				// TODO @seankimkdy: add SourceTags test once we start supporting tags
+			},
+			"fw-invisinets-permitlist-cc2ee3ff1e19a62b32c07e54c23c42ebbdb32": {
+				Allowed: []*computepb.Allowed{
+					{
+						IPProtocol: proto.String("17"),
+						Ports:      []string{},
+					},
+				},
+				DestinationRanges: []string{"10.3.4.0/24"},
+				Direction:         proto.String(computepb.Firewall_EGRESS.String()),
+				Name:              proto.String("fw-invisinets-permitlist-cc2ee3ff1e19a62b32c07e54c23c42ebbdb32"), // generated by getFirewallName
+				Network:           proto.String("nw-invisinets"),
+				TargetTags:        []string{fakeTag},
+			},
+			"fw-invisinets-permitlist-allow-ssh": {
+				Allowed: []*computepb.Allowed{
+					{
+						IPProtocol: proto.String("6"),
+						Ports:      []string{"22"},
+					},
+				},
+				Direction:  proto.String(computepb.Firewall_INGRESS.String()),
+				Name:       proto.String("fw-invisinets-permitlist-allow-ssh"),
+				Network:    proto.String("global/networks/default"),
+				TargetTags: []string{"0.0.0.0/0"},
+			},
+			"fw-allow-icmp": {
+				Allowed: []*computepb.Allowed{
+					{
+						IPProtocol: proto.String("1"),
+						Ports:      []string{},
+					},
+				},
+				Direction:  proto.String(computepb.Firewall_INGRESS.String()),
+				Name:       proto.String("fw-allow-icmp"),
+				Network:    proto.String("nw-invisinets"),
+				TargetTags: []string{"0.0.0.0/0"},
+			},
+		},
 	}
-})
+}
 
 func TestGetPermitList(t *testing.T) {
-	fakeServer := httptest.NewServer(fakeServerHandler)
+	fakeServerState := generateFakeServerState()
+	fakeServer := httptest.NewServer(getFakeServerHandler(fakeServerState))
 	defer fakeServer.Close()
 
 	ctx := context.Background()
@@ -169,11 +232,12 @@ func TestGetPermitList(t *testing.T) {
 		},
 	}
 
-	require.Equal(t, got, want)
+	require.True(t, proto.Equal(got, want))
 }
 
-func TestAddPermitListrules(t *testing.T) {
-	fakeServer := httptest.NewServer(fakeServerHandler)
+func TestAddPermitListRules(t *testing.T) {
+	fakeServerState := generateFakeServerState()
+	fakeServer := httptest.NewServer(getFakeServerHandler(fakeServerState))
 	defer fakeServer.Close()
 
 	ctx := context.Background()
@@ -201,23 +265,70 @@ func TestAddPermitListrules(t *testing.T) {
 		},
 	}
 
-	fmt.Printf(getFirewallName(permitList.Rules[0]))
-
-	prevFakeFirewallsCnt := len(fakeFirewalls)
+	prevFakeFirewallsCnt := len(fakeServerState.firewallMap)
 	resp, err := s._AddPermitListRules(ctx, permitList, fakeFirewallsClient, fakeInstancesClient)
+	if err != nil {
+		t.Fatal(err)
+	}
 	require.NotNil(t, resp)
 	require.True(t, resp.Success)
-	require.Equal(t, len(fakeFirewalls), prevFakeFirewallsCnt+1)
-	require.Equal(t, fakeFirewalls[len(fakeFirewalls)-1], &computepb.Firewall{
+	require.Equal(t, len(fakeServerState.firewallMap), prevFakeFirewallsCnt+1)
+	require.True(t, proto.Equal(fakeServerState.firewallMap["fw-invisinets-permitlist-afa8915cd5cadea6b17d59caf16df4deb8a36"], &computepb.Firewall{
 		Allowed: []*computepb.Allowed{
 			{
 				IPProtocol: proto.String("6"),
 				Ports:      []string{"443"},
 			},
 		},
-		Direction: proto.String(computepb.Firewall_INGRESS.String()),
-		Disabled:  proto.Bool(false),
-		Name:      proto.String("fw-invisinets-permitlist-afa8915cd5cadea6b17d59caf16df4deb8a36"),
-		Network:   proto.String("nw-invisinets"),
-	})
+		Description:  proto.String("Invisinets permit list"),
+		Direction:    proto.String(computepb.Firewall_INGRESS.String()),
+		Name:         proto.String("fw-invisinets-permitlist-afa8915cd5cadea6b17d59caf16df4deb8a36"),
+		Network:      proto.String("nw-invisinets"),
+		SourceRanges: []string{"10.5.6.0/24"},
+		TargetTags:   []string{fakeTag},
+	}))
+}
+
+func TestDeletePermitListRules(t *testing.T) {
+	fakeServerState := generateFakeServerState()
+	fakeServer := httptest.NewServer(getFakeServerHandler(fakeServerState))
+	defer fakeServer.Close()
+
+	ctx := context.Background()
+	fakeFirewallsClient, err := compute.NewFirewallsRESTClient(ctx, option.WithEndpoint(fakeServer.URL))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer fakeFirewallsClient.Close()
+	fakeInstancesClient, err := compute.NewInstancesRESTClient(ctx, option.WithEndpoint(fakeServer.URL))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer fakeInstancesClient.Close()
+
+	s := &GCPPluginServer{}
+	permitList := &invisinetspb.PermitList{
+		AssociatedResource: fakeResourceId,
+		Rules: []*invisinetspb.PermitListRule{
+			{
+				Direction: invisinetspb.Direction_INBOUND,
+				DstPort:   80,
+				Protocol:  6,
+				Tag:       []string{"10.1.2.0/24"},
+			},
+		},
+	}
+
+	prevFakeFirewallsCnt := len(fakeServerState.firewallMap)
+	resp, err := s._DeletePermitListRules(ctx, permitList, fakeFirewallsClient, fakeInstancesClient)
+	if err != nil {
+		t.Fatal(err)
+	}
+	require.NotNil(t, resp)
+	require.True(t, resp.Success)
+	require.Equal(t, len(fakeServerState.firewallMap), prevFakeFirewallsCnt-1)
+	_, ok := fakeServerState.firewallMap["fw-invisinets-permitlist-a48c81a983da600d848e5d35d8ad05cc90b41"]
+	if ok {
+		t.Errorf("desired firewall was not deleted")
+	}
 }
