51d0c95edda5bc240cbdba4ad14772e0c0ec1c3a
Noha Nomier <33196018+nnomier@users.noreply.github.com>
2023-07-31 13:53:44 -0700

    [Azure] Add mapping for Permit List API in azure plugin (#6)
    Add the necessary implementation for permit list logic in azure plugin
    
    **_Assumptions:_**
    - Only work with virtual machines resources
    - `AZURE_SUBSCRIPTION_ID` and `AZURE_RESOURCE_GROUP_NAME` are
    environment variables
    - User is logged in with `az login` to get the required credentials
    - The associated resource with the permit list is a correct azure
    resource uri
    - The tag associated with the PermitListRule is considered a valid IP
    address, it could be the source or destination IP depending on the rule
    direction (inbound or outbound)
    - An NSG rule with the name prefixed with "invisinets" is considered a
    permit list rule (Naming convention could change later to be global on
    all CP)
    - NSG is attached to a VM's primary NIC
    
    **Files added:**
    1-`azure_plugin_server.go` to create `azurePluginServer` that implements
    `invisinetspb.UnimplementedCloudPluginServer`, this should include
    implementation for all the required invisinets APIs
    2- `azure_sdk_helper.go`, this should include the more specific azure
    sdk functionalities.
    
    **Permit List Implementation**
    _All the following functions start by connecting to azure using
    `azidentity.NewDefaultAzureCredential(nil)` for auth and initializing
    the necessary SDK clients_
    
    **1- AddPermitListRules**
    - Input: permit list object that includes associated resource ID and a
    set of permit list rules to be added
    - Result: OK response if the rules are added successfully and NSG rules
    are created accordingly or an error if any error occurs during the steps
    
    **Steps:**
    - Get NSG associated to the resource from the NIC of the resource
    - If NSG doesn't exist create a new one and associate it to the resource
    NIC
    - Iterate over the permit list rules and eliminate any duplicates and
    for each NON duplicate rule create an equivalent NSG rule
    
    **2- DeletePermitListRules**
    - Input : permit list object that includes associated resource ID and a
    set of permit list rules to be deleted
    - Result: OK response if the rules are deleted successfully or an error
    if any error occurs during the steps
    
    **Steps:**
    - Get NSG associated to the resource from the NIC of the resource
    - Add Permit List Rules to be deleted in a set in the form of a string
    to simplify comparisons later
    - Iterate on the NSG rules and check for any NSG rule starting the
    invisinets prefix that matches the set of rules to be deleted
    - Delete the rules from Azure
    
    **3- GetPermitList**
    - Input: Resource ID 
    - Reult: return Permit List object that includes the rules associated to
    that resource
    
    **Steps:**
    - Get NSG associated to the resource from the NIC of the resource
    -  Initialize a Permit List object 
    - Iterate on the rules of the NSG and for each rule with the name
    starting with the invisinets prefix, create an equivalent Permit List
    Rule by doing some reverse mapping and add it to the Permit List object
    
    **Testing Overview:**
    This PR includes 2 test files:
    
    1. azure_plugin_server_test.go
    This one is responsible for testing azure_plugin_server and tests are
    done by mocking the azure_sdk_handler and manipulating the output as
    desired.
    2. azure_sdk_handler_test.go
    This one is responsible for testing azure_sdk_handler and since the
    azure_sdk_handler relies on azure sdk clients, the mocking is done by
    creating a fake server and rerouting the requests done by the clients to
    localhost (the fake server) instead of the real server, so no real calls
    are made, the main thing being tested is how the code handles
    successful/failed responses.
    
    branch: noha/azure-plugin

---
+++ pkg/azure_plugin/azure_sdk_handler.go
@@ -0,0 +1,385 @@
+/*
+Copyright 2023 The Invisinets Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package azure_plugin
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	invisinetspb "github.com/NetSys/invisinets/pkg/invisinetspb"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v4"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
+)
+
+type AzureSDKHandler interface {
+	CreateNetworkSecurityGroup(ctx context.Context, nsgName string, location string) (*armnetwork.SecurityGroup, error)
+	InitializeClients(cred azcore.TokenCredential)
+	GetAzureCredentials() (azcore.TokenCredential, error)
+	GetResourceNIC(ctx context.Context, resourceID string) (*armnetwork.Interface, error)
+	UpdateNetworkInterface(ctx context.Context, resourceNic *armnetwork.Interface, nsg *armnetwork.SecurityGroup) (*armnetwork.Interface, error)
+	CreateSecurityRule(ctx context.Context, rule *invisinetspb.PermitListRule, nsgName string, ruleName string, resourceIpAddress string, priority int32) (*armnetwork.SecurityRule, error)
+	DeleteSecurityRule(ctx context.Context, nsgName string, ruleName string) error
+	GetPermitListRuleFromNSGRule(rule *armnetwork.SecurityRule) (*invisinetspb.PermitListRule, error)
+	GetInvisinetsRuleDesc(rule *invisinetspb.PermitListRule) string
+	GetSecurityGroup(ctx context.Context, nsgName string) (*armnetwork.SecurityGroup, error)
+	GetLastSegment(resourceID string) (string, error)
+}
+
+type azureSDKHandler struct {
+	AzureSDKHandler
+	resourcesClientFactory *armresources.ClientFactory
+	computeClientFactory   *armcompute.ClientFactory
+	networkClientFactory   *armnetwork.ClientFactory
+	securityGroupsClient   *armnetwork.SecurityGroupsClient
+	interfacesClient       *armnetwork.InterfacesClient
+	securityRulesClient    *armnetwork.SecurityRulesClient
+	virtualMachinesClient  *armcompute.VirtualMachinesClient
+	resourcesClient        *armresources.Client
+	subscriptionID         string
+	resourceGroupName      string
+}
+
+const (
+	VirtualMachineResourceType = "Microsoft.Compute/virtualMachines"
+)
+
+// mapping from IANA protocol numbers (what invisinets uses) to Azure SecurityRuleProtocol except for * which is -1 for all protocols
+var invisinetsToAzureprotocol = map[int32]armnetwork.SecurityRuleProtocol{
+	256: armnetwork.SecurityRuleProtocolAsterisk,
+	1:   armnetwork.SecurityRuleProtocolIcmp,
+	6:   armnetwork.SecurityRuleProtocolTCP,
+	17:  armnetwork.SecurityRuleProtocolUDP,
+	50:  armnetwork.SecurityRuleProtocolEsp,
+	51:  armnetwork.SecurityRuleProtocolAh,
+}
+
+// mapping from Azure SecurityRuleProtocol to IANA protocol numbers
+var azureToInvisinetsProtocol = map[armnetwork.SecurityRuleProtocol]int32{
+	armnetwork.SecurityRuleProtocolAsterisk: 256,
+	armnetwork.SecurityRuleProtocolIcmp:     1,
+	armnetwork.SecurityRuleProtocolTCP:      6,
+	armnetwork.SecurityRuleProtocolUDP:      17,
+	armnetwork.SecurityRuleProtocolEsp:      50,
+	armnetwork.SecurityRuleProtocolAh:       51,
+}
+
+// mapping from invisinets direction to Azure SecurityRuleDirection
+var invisinetsToAzureDirection = map[invisinetspb.Direction]armnetwork.SecurityRuleDirection{
+	invisinetspb.Direction_INBOUND:  armnetwork.SecurityRuleDirectionInbound,
+	invisinetspb.Direction_OUTBOUND: armnetwork.SecurityRuleDirectionOutbound,
+}
+
+// mapping from Azure SecurityRuleDirection to invisinets direction
+var azureToInvisinetsDirection = map[armnetwork.SecurityRuleDirection]invisinetspb.Direction{
+	armnetwork.SecurityRuleDirectionInbound:  invisinetspb.Direction_INBOUND,
+	armnetwork.SecurityRuleDirectionOutbound: invisinetspb.Direction_OUTBOUND,
+}
+
+// CreateNetworkSecurityGroup creates a new network security group with the given name and location
+// and returns the created network security group
+func (h *azureSDKHandler) CreateNetworkSecurityGroup(ctx context.Context, nsgName string, location string) (*armnetwork.SecurityGroup, error) {
+	log.Printf("creating a new network security group %s in location %s", nsgName, location)
+	parameters := armnetwork.SecurityGroup{
+		Location: to.Ptr(location),
+		Properties: &armnetwork.SecurityGroupPropertiesFormat{
+			SecurityRules: []*armnetwork.SecurityRule{},
+		},
+	}
+	pollerResponse, err := h.securityGroupsClient.BeginCreateOrUpdate(ctx, h.resourceGroupName, nsgName, parameters, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := pollerResponse.PollUntilDone(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.SecurityGroup, nil
+}
+
+// InitializeClients initializes the necessary azure clients for the necessary operations
+func (h *azureSDKHandler) InitializeClients(cred azcore.TokenCredential) {
+	var err error
+	h.resourcesClientFactory, err = armresources.NewClientFactory(h.subscriptionID, cred, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	h.networkClientFactory, err = armnetwork.NewClientFactory(h.subscriptionID, cred, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	h.computeClientFactory, err = armcompute.NewClientFactory(h.subscriptionID, cred, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	h.securityGroupsClient = h.networkClientFactory.NewSecurityGroupsClient()
+	h.interfacesClient = h.networkClientFactory.NewInterfacesClient()
+	h.securityRulesClient = h.networkClientFactory.NewSecurityRulesClient()
+	h.resourcesClient = h.resourcesClientFactory.NewClient()
+	h.virtualMachinesClient = h.computeClientFactory.NewVirtualMachinesClient()
+}
+
+// GetAzureCredentials returns an Azure credential.
+// it uses the azidentity.NewDefaultAzureCredential() function to create a new Azure credential.
+func (h *azureSDKHandler) GetAzureCredentials() (azcore.TokenCredential, error) {
+	h.subscriptionID = os.Getenv("AZURE_SUBSCRIPTION_ID")
+	h.resourceGroupName = os.Getenv("AZURE_RESOURCE_GROUP_NAME")
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		return nil, err
+	}
+	return cred, nil
+}
+
+// GetResourceNIC returns the network interface card (NIC) for a given resource ID.
+// it performs the following steps:
+// 1. Get the resource by ID using the resourcesClient.GetByID() function.
+// 2. If the resource is a virtual machine, get the virtual machine by name using the virtualMachinesClient.Get() function.
+// 3. Get the primary NIC ID from the virtual machine's network profile and extract the NIC name from it.
+// 4. Get the NIC by name using the interfacesClient.Get() function and set the return value to the NIC object.
+func (h *azureSDKHandler) GetResourceNIC(ctx context.Context, resourceID string) (*armnetwork.Interface, error) {
+	var resourceNic *armnetwork.Interface
+	var apiVersion string = "2021-04-01"
+	options := armresources.ClientGetByIDOptions{}
+
+	// TODO @nnomier: if we just use VMs, we can use vmclient directly
+	resource, err := h.resourcesClient.GetByID(ctx, resourceID, apiVersion, &options)
+	if err != nil {
+		log.Printf("Failed to get resource: %v", err)
+		return nil, err
+	}
+
+	//TODO @nnomier: Do a solution that should work for all types
+	if *resource.Type != VirtualMachineResourceType {
+		err := fmt.Errorf("resource type %s is not supported", *resource.Type)
+		return nil, err
+	}
+
+	vmName := *resource.Name
+
+	// get the VM
+	vm, err := h.virtualMachinesClient.Get(ctx, h.resourceGroupName, vmName, &armcompute.VirtualMachinesClientGetOptions{Expand: nil})
+
+	if err != nil {
+		log.Printf("Failed to get VM: %v", err)
+		return nil, err
+	}
+
+	// get the primary NIC ID from the VM
+	nicID := *vm.Properties.NetworkProfile.NetworkInterfaces[0].ID
+	nicName, err := h.GetLastSegment(nicID)
+	if err != nil {
+		log.Printf("Failed to get NIC name from ID: %v", err)
+		return nil, err
+	}
+
+	nicResponse, err := h.interfacesClient.Get(ctx, h.resourceGroupName, nicName, &armnetwork.InterfacesClientGetOptions{Expand: nil})
+	if err != nil {
+		log.Printf("Failed to get NIC: %v", err)
+		return nil, err
+	}
+	resourceNic = &nicResponse.Interface
+	return resourceNic, nil
+}
+
+// UpdateNetworkInterface updates a network interface card (NIC) with a new network security group (NSG).
+func (h *azureSDKHandler) UpdateNetworkInterface(ctx context.Context, resourceNic *armnetwork.Interface, nsg *armnetwork.SecurityGroup) (*armnetwork.Interface, error) {
+	pollerResp, err := h.interfacesClient.BeginCreateOrUpdate(
+		ctx,
+		h.resourceGroupName,
+		*resourceNic.Name,
+		armnetwork.Interface{
+			Location: resourceNic.Location,
+			Properties: &armnetwork.InterfacePropertiesFormat{
+				IPConfigurations:     resourceNic.Properties.IPConfigurations,
+				NetworkSecurityGroup: nsg,
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := pollerResp.PollUntilDone(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	nic := &resp.Interface
+	jsonData, err := json.MarshalIndent(nic, "", "  ")
+	if err != nil {
+		log.Printf("failed to marshal response to JSON: %v", err)
+		return nil, err
+	}
+	log.Printf("Successfully Updated Resource NIC: %v", string(jsonData))
+
+	return nic, nil
+}
+
+// getLastSegment returns the last segment of a resource ID.
+func (h *azureSDKHandler) GetLastSegment(ID string) (string, error) {
+	// TODO @nnomier: might need to use stricter validations to check if the ID is valid like a regex
+	segments := strings.Split(ID, "/")
+	// The smallest possible len would be 1 because in go if a string s does not contain sep and sep is not empty,
+	// Split returns a slice of length 1 whose only element is s.
+	if len(segments) <= 1 {
+		return "", fmt.Errorf("invalid resource ID format")
+	}
+	return segments[len(segments)-1], nil
+}
+
+// CreateSecurityRule creates a new security rule in a network security group (NSG).
+func (h *azureSDKHandler) CreateSecurityRule(ctx context.Context, rule *invisinetspb.PermitListRule, nsgName string, ruleName string, resourceIpAddress string, priority int32) (*armnetwork.SecurityRule, error) {
+	sourceIP, destIP := getIPs(rule, resourceIpAddress)
+	pollerResp, err := h.securityRulesClient.BeginCreateOrUpdate(ctx,
+		h.resourceGroupName,
+		nsgName,
+		ruleName,
+		armnetwork.SecurityRule{
+			Properties: &armnetwork.SecurityRulePropertiesFormat{
+				Access:                     to.Ptr(armnetwork.SecurityRuleAccessAllow),
+				DestinationAddressPrefixes: destIP,
+				DestinationPortRange:       to.Ptr(strconv.Itoa(int(rule.DstPort))),
+				Direction:                  to.Ptr(invisinetsToAzureDirection[rule.Direction]),
+				Priority:                   to.Ptr(priority),
+				Protocol:                   to.Ptr(invisinetsToAzureprotocol[rule.Protocol]),
+				SourceAddressPrefixes:      sourceIP,
+				SourcePortRange:            to.Ptr(strconv.Itoa(int(rule.SrcPort))),
+			},
+		},
+		nil)
+
+	if err != nil {
+		return nil, fmt.Errorf("cannot create HTTP security rule: %v", err)
+	}
+
+	resp, err := pollerResp.PollUntilDone(ctx, nil)
+	if err != nil {
+		return nil, fmt.Errorf("cannot get security rule create or update future response: %v", err)
+	}
+
+	return &resp.SecurityRule, nil
+}
+
+// DeleteSecurityRule deletes a security rule from a network security group (NSG).
+func (h *azureSDKHandler) DeleteSecurityRule(ctx context.Context, nsgName string, ruleName string) error {
+	pollerResp, err := h.securityRulesClient.BeginDelete(ctx, h.resourceGroupName, nsgName, ruleName, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := pollerResp.PollUntilDone(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	// resp of type SecurityRulesClientDeleteResponse is currently a placeholder in the sdk
+	log.Printf("Successfully deleted security rule: %v", resp)
+	return nil
+}
+
+// GetPermitListRuleFromNSGRule returns a permit list rule from a network security group (NSG) rule.
+func (h *azureSDKHandler) GetPermitListRuleFromNSGRule(rule *armnetwork.SecurityRule) (*invisinetspb.PermitListRule, error) {
+	srcPort, err := strconv.Atoi(*rule.Properties.SourcePortRange)
+	if err != nil {
+		return nil, err
+	}
+	dstPort, err := strconv.Atoi(*rule.Properties.DestinationPortRange)
+	if err != nil {
+		return nil, err
+	}
+	// create permit list rule object
+	permitListRule := &invisinetspb.PermitListRule{
+		Id:        *rule.ID,
+		Tag:       getTag(rule),
+		Direction: azureToInvisinetsDirection[*rule.Properties.Direction],
+		SrcPort:   int32(srcPort),
+		DstPort:   int32(dstPort),
+		Protocol:  azureToInvisinetsProtocol[*rule.Properties.Protocol],
+	}
+	return permitListRule, nil
+}
+
+// GetNSGRuleDesc returns a description of an invisinets permit list rule for easier comparison
+func (h *azureSDKHandler) GetInvisinetsRuleDesc(rule *invisinetspb.PermitListRule) string {
+	return fmt.Sprintf("%s-%d-%d-%d-%d", strings.Join(rule.Tag, "-"), rule.Direction, rule.SrcPort, rule.DstPort, rule.Protocol)
+}
+
+// GetSecurityGroup reutrns the network security group object given the nsg name
+func (h *azureSDKHandler) GetSecurityGroup(ctx context.Context, nsgName string) (*armnetwork.SecurityGroup, error) {
+	nsgResp, err := h.securityGroupsClient.Get(ctx, h.resourceGroupName, nsgName, &armnetwork.SecurityGroupsClientGetOptions{Expand: nil})
+	if err != nil {
+		return nil, err
+	}
+
+	return &nsgResp.SecurityGroup, nil
+}
+
+// getIPs returns the source and destination IP addresses for a given permit list rule and resource IP address.
+// it checks the direction of the permit list rule and sets the source IP address to the rule tag
+// and the destination IP address to the resource IP address if the direction is inbound.
+// If the direction is outbound, it sets the source IP address to the resource IP address and
+// the destination IP address to the rule tag.
+func getIPs(rule *invisinetspb.PermitListRule, resourceIP string) ([]*string, []*string) {
+	var sourceIP []*string
+	var destIP []*string
+
+	if rule.Direction == invisinetspb.Direction_INBOUND {
+		sourceIP = make([]*string, len(rule.Tag))
+		for i, ip := range rule.Tag {
+			sourceIP[i] = to.Ptr(ip)
+		}
+		destIP = []*string{to.Ptr(resourceIP)}
+	} else {
+		sourceIP = []*string{to.Ptr(resourceIP)}
+		destIP = make([]*string, len(rule.Tag))
+		for i, ip := range rule.Tag {
+			destIP[i] = to.Ptr(ip)
+		}
+	}
+
+	return sourceIP, destIP
+}
+
+// getTag returns the invisiNets tag for a given nsg rule
+func getTag(rule *armnetwork.SecurityRule) []string {
+	var tag []string
+	if *rule.Properties.Direction == armnetwork.SecurityRuleDirectionInbound {
+		for _, ptr := range rule.Properties.SourceAddressPrefixes {
+			tag = append(tag, *ptr)
+		}
+	} else if *rule.Properties.Direction == armnetwork.SecurityRuleDirectionOutbound {
+		for _, ptr := range rule.Properties.DestinationAddressPrefixes {
+			tag = append(tag, *ptr)
+		}
+	}
+	return tag
+}
