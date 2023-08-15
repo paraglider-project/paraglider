2f04f06eedc4eb0a995ea66aad35c05cc0c9ba06
Sean Kim <kimsean@google.com>
2023-08-15 19:29:07 +0000

    Use run numbers for unique resource names

--- pkg/gcp/plugin_test.go
+++ pkg/gcp/plugin_test.go
@@ -74,7 +74,7 @@ var (
 		},
 		Direction:    proto.String(computepb.Firewall_INGRESS.String()),
 		Name:         proto.String(getFirewallName(fakePermitListRule1)),
-		Network:      proto.String(vpcURL),
+		Network:      proto.String(getVPCURL()),
 		SourceRanges: []string{"10.1.2.0/24"},
 		TargetTags:   []string{fakeNetworkTag},
 	}
@@ -94,7 +94,7 @@ var (
 		DestinationRanges: []string{"10.3.4.0/24"},
 		Direction:         proto.String(computepb.Firewall_EGRESS.String()),
 		Name:              proto.String(getFirewallName(fakePermitListRule2)),
-		Network:           proto.String(vpcURL),
+		Network:           proto.String(getVPCURL()),
 		TargetTags:        []string{fakeNetworkTag},
 	}
 )
@@ -309,7 +309,7 @@ func TestGetPermitList(t *testing.T) {
 				},
 				Direction:  proto.String(computepb.Firewall_INGRESS.String()),
 				Name:       proto.String("fw-allow-icmp"),
-				Network:    proto.String(vpcURL),
+				Network:    proto.String(getVPCURL()),
 				TargetTags: []string{"0.0.0.0/0"},
 			},
 		},
