2c2c65dbd680a3aa7ec33020219b5422bb8aa6f6
Pravein-Govindan-Kannan <Pravein.Govindan.Kannan@ibm.com>
2023-10-18 13:46:44 -0700

    Add copyright headers to ibm plugin files
    Signed-off-by: Pravein-Govindan-Kannan <Pravein.Govindan.Kannan@ibm.com>

--- pkg/ibm_plugin/sdk/instance.go
+++ pkg/ibm_plugin/sdk/instance.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2023 The Invisinets Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package ibm
 
 import (
@@ -112,10 +128,10 @@ func (c *IBMCloudClient) createVM(
 }
 
 // return security group ids that are associated with the VM's network interfaces
-func (c *IBMCloudClient) GetInstanceSecurityGroups(vmName string) ([]string, error) {
+func (c *IBMCloudClient) GetInstanceSecurityGroups(name string) ([]string, error) {
 	var sgGroups []string
 
-	vmID, err := c.GetInstanceID(vmName)
+	vmID, err := c.GetInstanceID(name)
 	if err != nil {
 		return nil, err
 	}
