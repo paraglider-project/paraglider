bc60dff961a852350a2d26513b9c75dd9482dede
Cohen-J-Omer <cohen.j.omer@gmail.com>
2024-01-22 23:22:37 +0200

    incorporate the transit gateway service to sdk handler

--- pkg/ibm_plugin/sdk/sdk_handler.go
+++ pkg/ibm_plugin/sdk/sdk_handler.go
@@ -18,14 +18,13 @@ package ibm
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
-	"time"
 
 	"github.com/IBM/go-sdk-core/v5/core"
+	"github.com/IBM/networking-go-sdk/transitgatewayapisv1"
 	"github.com/IBM/platform-services-go-sdk/globalsearchv2"
 	"github.com/IBM/platform-services-go-sdk/globaltaggingv1"
 	"github.com/IBM/vpc-go-sdk/vpcv1"
+	ibmCommon "github.com/NetSys/invisinets/pkg/ibm_plugin"
 
 	utils "github.com/NetSys/invisinets/pkg/utils"
 )
@@ -37,6 +36,7 @@ type CloudClient struct {
 	globalSearch   *globalsearchv2.GlobalSearchV2
 	taggingService *globaltaggingv1.GlobalTaggingV1
 	resourceGroup  *vpcv1.ResourceGroupIdentityByID
+	transitGW      *transitgatewayapisv1.TransitGatewayApisV1
 }
 
 func (c *CloudClient) Region() string {
@@ -54,8 +54,8 @@ func (c *CloudClient) UpdateRegion(region string) error {
 }
 
 // returns CloudClient instance with initialized clients
-func NewIBMCloudClient(name, region string) (*CloudClient, error) {
-	if isRegionValid, err := IsRegionValid(region); !isRegionValid || err != nil {
+func NewIBMCloudClient(resourceGroupName, region string) (*CloudClient, error) {
+	if isRegionValid, err := ibmCommon.IsRegionValid(region); !isRegionValid || err != nil {
 		return nil, fmt.Errorf("region %v isn't valid", region)
 	}
 	creds, err := getIBMCred()
@@ -91,12 +91,22 @@ func NewIBMCloudClient(name, region string) (*CloudClient, error) {
 		return nil, err
 	}
 
-	resourceGroupID, err := getResourceID(authenticator, name)
+	resourceGroupID, err := getResourceID(authenticator, resourceGroupName)
 	if err != nil {
 		return nil, err
 	}
 
 	resourceGroupIdentity := &vpcv1.ResourceGroupIdentityByID{ID: resourceGroupID}
+
+	transitOptions := &transitgatewayapisv1.TransitGatewayApisV1Options{
+		Version:       core.StringPtr("2023-12-05"), // version is a mandatory field
+		Authenticator: authenticator,
+	}
+
+	transitGatewayService, err := transitgatewayapisv1.NewTransitGatewayApisV1(transitOptions)
+	if err != nil {
+		return nil, err
+	}
 
 	client := CloudClient{
 		vpcService:     api,
@@ -104,117 +114,7 @@ func NewIBMCloudClient(name, region string) (*CloudClient, error) {
 		globalSearch:   globalSearch,
 		taggingService: taggingService,
 		resourceGroup:  resourceGroupIdentity,
+		transitGW:      transitGatewayService,
 	}
 	return &client, nil
-}
-
-func (c *CloudClient) attachTag(CRN *string, tags []string) error {
-	tags = append(tags, InvTag) // add universal tag for invisinets' resources
-	userTypeTag := globaltaggingv1.AttachTagOptionsTagTypeUserConst
-	resourceModel := &globaltaggingv1.Resource{
-		ResourceID:   CRN,
-		ResourceType: &userTypeTag,
-	}
-	attachTagOptions := c.taggingService.NewAttachTagOptions(
-		[]globaltaggingv1.Resource{*resourceModel},
-	)
-	attachTagOptions.SetTagNames(tags)
-
-	maxAttempts := 10    // retries number to tag a resource
-	latestResponse := "" // record latest response from inner scope
-	for attempt := 1; attempt <= maxAttempts; attempt += 1 {
-		result, latestResponse, _ := c.taggingService.AttachTag(attachTagOptions)
-		// tracking all responses from error prone tagging service
-		utils.Log.Printf("Tagging attempt %v: Response: %+v", attempt, latestResponse)
-		if !*result.Results[0].IsError {
-			return nil
-		}
-		time.Sleep(5 * time.Second)
-	}
-	return fmt.Errorf("Failed to tag resource with response:\n %+v", latestResponse)
-}
-
-// GetInvisinetsTaggedResources returns slice of IDs of tagged resources
-// Arg resourceType: type of VPC resource, e.g. subnet, security group, instance.
-// Arg tags: labels set by dev, e.g. {<vpcID>,<deploymentID>}
-// Args customQueryMap: map of attributes to filter by, e.g. {"region":"<regionName>"}
-func (c *CloudClient) GetInvisinetsTaggedResources(resourceType TaggedResourceType, tags []string,
-	customQuery ResourceQuery) ([]ResourceData, error) {
-	// parse tags
-	var tagsStr string
-	var queryStr string
-	// append the invisinets tag to narrow the search scope to invisinets resources only.
-	tags = append(tags, InvTag)
-	for _, tag := range tags {
-		tagsStr += fmt.Sprintf("tags:%v AND ", tag)
-	}
-	tagsStr = strings.TrimSuffix(tagsStr, "AND ") // remove last operator
-
-	// Parse region/zone individually to accommodate unique behavior:
-	// globalSearch returns the region field for all resources, although for some it holds
-	// the zone value, e.g. subnet's region actually contain a zone's value.
-	if customQuery.Zone != "" {
-		queryStr += fmt.Sprintf("%v:%v AND ", "region", customQuery.Zone)
-	} else if customQuery.Region != "" {
-		queryStr += fmt.Sprintf("%v:%v* AND ", "region", customQuery.Region)
-		// e.g. region:eu-de* would fetch all zones in eu-de for zone bound resources
-	}
-
-	if customQuery.CRN != "" {
-		queryStr += fmt.Sprintf("%v:\"%v\" ", "crn", customQuery.CRN)
-	}
-
-	resourceList, err := c.getInvisinetsResourceByTags(string(resourceType), tagsStr, queryStr)
-	if err != nil {
-		return nil, err
-	}
-	return resourceList, nil
-}
-
-// returns IDs of resources filtered by tags and query
-func (c *CloudClient) getInvisinetsResourceByTags(resourceType string,
-	tags string, customQueryStr string) ([]ResourceData, error) {
-	var taggedResources []ResourceData
-
-	query := fmt.Sprintf("type:%v AND %v ", resourceType, tags)
-	if customQueryStr != "" {
-		query += "AND " + customQueryStr
-	}
-
-	searchOptions := c.globalSearch.NewSearchOptions()
-	searchOptions.SetLimit(100)
-	searchOptions.SetQuery(query)
-	searchOptions.SetFields([]string{"*"})
-
-	result, response, err := c.globalSearch.Search(searchOptions)
-	if err != nil {
-		utils.Log.Println("tags search was invalid. Response\n", response)
-		return nil, err
-	}
-	items := result.Items
-	if len(items) != 0 {
-		for _, item := range items {
-			resData := ResourceData{}
-			id := CRN2ID(*item.CRN)
-			itemProperties := item.GetProperties()
-			if _, regionExists := itemProperties["region"]; regionExists {
-				region := itemProperties["region"].(string)
-				if len(region) != 0 {
-					regionParts := strings.Split(region, "-")
-					// since globalSearch.Search stores both region and zone in the region field,
-					// we need to verify what the value represents
-					if _, err := strconv.Atoi(regionParts[len(regionParts)-1]); err != nil {
-						resData.Region = region
-					} else {
-						resData.Zone = region
-					}
-				}
-			}
-			resData.ID = id
-			taggedResources = append(taggedResources, resData)
-		}
-	} else { // no resources found with specified tags
-		return nil, nil
-	}
-	return taggedResources, nil
 }
