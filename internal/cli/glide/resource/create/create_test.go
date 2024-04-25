ea7b83c5933e4f61d55b045e7e8d3ecac36080b5
smcclure20 <42726127+smcclure20@users.noreply.github.com>
2024-04-24 17:29:42 -0700

    [CLI] Rename to glide (#234)
    * Renames the CLI itself [#208]
    * Replaces instance of "inv" in the API reference doc
    * TODO: replace instances of "inv" in the quickstart and example docs
    (waiting until their active PRs are merged)

---
+++ internal/cli/glide/resource/create/create_test.go
@@ -0,0 +1,57 @@
+//go:build unit
+
+/*
+Copyright 2023 The Invisinets Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package create
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/NetSys/invisinets/internal/cli/glide/settings"
+	fake "github.com/NetSys/invisinets/pkg/fake/orchestrator/rest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestResourceCreateValidate(t *testing.T) {
+	cmd, executor := NewCommand()
+
+	args := []string{fake.CloudName, "uri", "not-a-file.json"}
+	err := executor.Validate(cmd, args)
+
+	assert.NotNil(t, err)
+
+	// TODO @smcclure20: add more test cases
+}
+
+func TestResourceCreateExecute(t *testing.T) {
+	server := &fake.FakeOrchestratorRESTServer{}
+	serverAddr := server.SetupFakeOrchestratorRESTServer()
+
+	cmd, executor := NewCommand()
+	executor.cliSettings = settings.CLISettings{ServerAddr: serverAddr, ActiveNamespace: fake.Namespace}
+
+	var output bytes.Buffer
+	executor.writer = &output
+	executor.description = []byte(`descriptionstring`)
+
+	args := []string{fake.CloudName, "resourceName"}
+	err := executor.Execute(cmd, args)
+
+	assert.Nil(t, err)
+	assert.Contains(t, output.String(), "resourceName")
+}
---
+++ internal/cli/glide/tag/delete/delete.go
@@ -0,0 +1,72 @@
+/*
+Copyright 2023 The Invisinets Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package delete
+
+import (
+	"io"
+	"os"
+
+	common "github.com/NetSys/invisinets/internal/cli/common"
+	"github.com/NetSys/invisinets/internal/cli/glide/settings"
+	"github.com/NetSys/invisinets/pkg/client"
+	"github.com/spf13/cobra"
+)
+
+func NewCommand() (*cobra.Command, *executor) {
+	executor := &executor{writer: os.Stdout, cliSettings: settings.Global}
+	cmd := &cobra.Command{
+		Use:     "delete <tag name> [-member <member>]",
+		Short:   "Delete a tag",
+		Args:    cobra.ExactArgs(1),
+		PreRunE: executor.Validate,
+		RunE:    executor.Execute,
+	}
+	cmd.Flags().String("member", "", "The member to delete")
+	return cmd, executor
+}
+
+type executor struct {
+	common.CommandExecutor
+	cliSettings settings.CLISettings
+	writer      io.Writer
+	member      string
+}
+
+func (e *executor) SetOutput(w io.Writer) {
+	e.writer = w
+}
+
+func (e *executor) Validate(cmd *cobra.Command, args []string) error {
+	var err error
+	e.member, err = cmd.Flags().GetString("member")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (e *executor) Execute(cmd *cobra.Command, args []string) error {
+	// Delete the tag from the server
+	c := client.Client{ControllerAddress: e.cliSettings.ServerAddr}
+	if e.member == "" {
+		err := c.DeleteTag(args[0])
+		return err
+	} else {
+		err := c.DeleteTagMembers(args[0], e.member)
+		return err
+	}
+}
