a08089b1bddadfa935c622391888dcafe26c7e7b
Pravein Govindan Kannan <pravein.govindan.kannan@ibm.com>
2025-05-16 00:28:54 +0530

    Modify for version v0.1.0 (#518)
    Signed-off-by: Pravein-Govindan-Kannan <Pravein.Govindan.Kannan@ibm.com>

--- build/version.mk
+++ build/version.mk
@@ -1,8 +1,8 @@
 # Commit and release info gets compiled into the binary at build time.
 GIT_COMMIT  = $(shell git rev-list -1 HEAD)
-GIT_VERSION = $(shell git describe --always --abbrev=7 --dirty --tags)
-REL_VERSION ?= latest
-REL_CHANNEL ?= latest
+GIT_VERSION = $(shell git describe --always --abbrev=7 --tags)
+REL_VERSION ?= 0.1.0
+REL_CHANNEL ?= 0.1.0
 
 # These flags are passed to the linker and will configure the version information in the build.
 VERSION_LD_FLAGS = -X $(BASE_PACKAGE_NAME)/internal/version.channel=$(REL_CHANNEL) 
